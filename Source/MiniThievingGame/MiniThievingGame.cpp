42b643770001194c5c9bdb4c9b15b1b43aba5f6a
Bisher Dokkmak <bisher.d790@gmail.com>
2024-07-28 02:48:45 +0700

    [Init Project]

---
+++ Source/MiniThievingGame/MiniThievingGame.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MiniThievingGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MiniThievingGame, "MiniThievingGame" );
+
+DEFINE_LOG_CATEGORY(LogMiniThievingGame)
+ 
